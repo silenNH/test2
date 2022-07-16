4068a4dbb6b46e806e5e94e152a0a0a4801675da
Niels Humbeck <nielshumbeck91@googlemail.com>
2022-07-16 20:49:24 +0200

    TestUnittest

--- test_calc.py
+++ test_calc.py
@@ -7,7 +7,3 @@ class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(-1, 1), 0)
         self.assertEqual(calc.add(-1, -1), -2)
-
-
-#if __name__ == '__main__':
-#    unittest.main()
